d0db397ddbe0e65d7d24f9979cf06d6e8fdbdc7e
LeonRein <4107480+LeonRein@users.noreply.github.com>
2025-01-09 16:43:45 +0100

    changed wezterm font to iosevka

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,11 +20,9 @@ config.window_padding = {
 
 -- font config
 config.font_size = 12
-config.font = wezterm.font(
-  { -- Normal text
-  family='Monaspace Neon',
-  harfbuzz_features={ 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' },
-  stretch='UltraCondensed', -- This doesn't seem to do anything
+config.font = wezterm.font({
+  family='Iosevka',
+  harfbuzz_features={ 'dlig'},
 })
 
 -- and finally, return the configuration to wezterm
